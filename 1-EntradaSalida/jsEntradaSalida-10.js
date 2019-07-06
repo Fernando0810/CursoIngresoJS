fb6ab809a6025310ee81f309bb4fc377594ae181
Fernando0810 <52495399+Fernando0810@users.noreply.github.com>
2019-07-06 12:47:35 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,23 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+var Importe;
+var Resultado;
+var nuevoSueldo;
+
+Importe=document.getElementById("importe").value;
+Importe=parseInt(Importe);
+
+Resultado=(Importe*25)/100;
+
+nuevoSueldo=Importe-Resultado;
+
+document.getElementById("resultado").value=nuevoSueldo;
+
+
+
+
+
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,33 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	
+ var Importe;
+ var Resultado;
+ var nuevoImporte;
+
+ Importe=document.getElementById("sueldo").value;
+ Importe=parseInt(Importe);
+
+ Resultado=(Importe*10)/100;
+
+ nuevoImporte=Resultado + Importe;
+
+  document.getElementById("resultado").value=nuevoImporte;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
